75055cc6435947e4c4388d5d48e1988919803086
Akib Uz Zaman <akibuz@synopsys.com>
2023-06-16 14:59:21 +0600

    add initial security scan step

---
+++ src/main/java/com/synopsys/integration/jenkins/scan/SecurityScanStep.java
@@ -0,0 +1,99 @@
+package com.synopsys.integration.jenkins.scan;
+
+import com.synopsys.integration.jenkins.scan.global.ApplicationConstants;
+
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+
+import org.jenkinsci.plugins.workflow.steps.Step;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
+import org.jenkinsci.plugins.workflow.steps.SynchronousNonBlockingStepExecution;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author akib @Date 6/15/23
+ */
+public class SecurityScanStep extends Step implements Serializable {
+
+    private static final long serialVersionUID = 6294070801130995534L;
+    private final String param1;
+    private final String param2;
+
+    @DataBoundConstructor
+    public SecurityScanStep(String param1, String param2) {
+        this.param1 = param1;
+        this.param2 = param2;
+    }
+
+    public String getParam1() {
+        return param1;
+    }
+
+    public String getParam2() {
+        return param2;
+    }
+
+    @Override
+    public StepExecution start(StepContext context) throws Exception {
+        return new Execution(context);
+    }
+
+    @Extension(optional = true)
+    public static final class DescriptorImpl extends StepDescriptor {
+        @Override
+        public Set<? extends Class<?>> getRequiredContext() {
+            return new HashSet<>(Arrays.asList(TaskListener.class, EnvVars.class, FilePath.class, Launcher.class, Node.class));
+        }
+
+        @Override
+        public String getFunctionName() {
+            return ApplicationConstants.PIPELINE_NAME;
+        }
+
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return ApplicationConstants.DISPLAY_NAME;
+        }
+
+    }
+
+    public class Execution extends SynchronousNonBlockingStepExecution<Integer> {
+        private static final long serialVersionUID = -2514079516220990421L;
+        private final transient TaskListener listener;
+        private final transient EnvVars envVars;
+        private final transient FilePath workspace;
+        private final transient Launcher launcher;
+        private final transient Node node;
+
+        protected Execution(@Nonnull StepContext context) throws InterruptedException, IOException {
+            super(context);
+            listener = context.get(TaskListener.class);
+            envVars = context.get(EnvVars.class);
+            workspace = context.get(FilePath.class);
+            launcher = context.get(Launcher.class);
+            node = context.get(Node.class);
+        }
+
+        @Override
+        protected Integer run() throws IOException, InterruptedException {
+            return 0;
+        }
+
+    }
+
+}
---
+++ src/main/java/com/synopsys/integration/jenkins/scan/global/ApplicationConstants.java
@@ -0,0 +1,21 @@
+package com.synopsys.integration.jenkins.scan.global;
+/**
+ * @author akib @Date 6/15/23
+ */
+public class ApplicationConstants {
+
+    public static final String APPLICATION_NAME = "synopsys-security-scan";
+    public static final String DISPLAY_NAME = "Synopsys Scan";
+    public static final String PIPELINE_NAME = "synopsys_scan";
+    public static final String BRIDGE_ARTIFACTORY_URL = "https://sig-repo.synopsys.com/artifactory/bds-integrations-release/com/synopsys/integration/synopsys-bridge/";
+    public static final String SYNOPSYS_BRIDGE_DEFAULT_PATH = "/synopsys-bridge";
+    public static final String SYNOPSYS_BRIDGE_LATEST_VERSION = "latest";
+    public static final String PLATFORM_LINUX = "linux64";
+    public static final String PLATFORM_MAC = "macosx";
+    public static final String PLATFORM_WINDOWS = "win64";
+
+    public static final String getSynopsysBridgeZipFileName(String platform) {
+        return "synopsys-bridge-" + platform + ".zip";
+    }
+
+}
