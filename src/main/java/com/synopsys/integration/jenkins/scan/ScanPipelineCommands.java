4755ca0213aa7cd5b6a06c3b15d5316090caa339
Akib Uz Zaman <akibuz@synopsys.com>
2023-08-23 12:38:11 +0600

    move bridge related validations to ScanPipelineCommands

--- src/main/java/com/synopsys/integration/jenkins/scan/ScanPipelineCommands.java
+++ src/main/java/com/synopsys/integration/jenkins/scan/ScanPipelineCommands.java
@@ -1,28 +1,52 @@
 package com.synopsys.integration.jenkins.scan;
 
+import com.synopsys.integration.jenkins.scan.bridge.BridgeDownloadManager;
+import com.synopsys.integration.jenkins.scan.bridge.BridgeDownloadParameters;
 import com.synopsys.integration.jenkins.scan.exception.ScannerJenkinsException;
 import com.synopsys.integration.jenkins.scan.global.ExceptionMessages;
+import com.synopsys.integration.jenkins.scan.service.BridgeDownloadParametersService;
 import com.synopsys.integration.jenkins.scan.service.scan.ScanStrategyFactory;
 import com.synopsys.integration.jenkins.scan.service.scan.ScanStrategyService;
+import hudson.FilePath;
+import hudson.model.TaskListener;
 import java.util.Map;
 
 public class ScanPipelineCommands {
     private final SecurityScanner scanner;
+    private final FilePath workspace;
+    private final TaskListener listener;
 
-    public ScanPipelineCommands(SecurityScanner scanner) {
+    public ScanPipelineCommands(SecurityScanner scanner, FilePath workspace, TaskListener listener) {
         this.scanner = scanner;
+        this.workspace = workspace;
+        this.listener = listener;
     }
 
     public int runScanner(Map<String, Object> scanParameters, ScanStrategyFactory scanStrategyFactory) throws ScannerJenkinsException {
         ScanStrategyService scanStrategyService = scanStrategyFactory.getParametersService(scanParameters);
 
+        BridgeDownloadParameters bridgeDownloadParameters = new BridgeDownloadParameters(workspace, listener);
+        BridgeDownloadParametersService bridgeDownloadParametersService = new BridgeDownloadParametersService(workspace, listener);
+        BridgeDownloadParameters bridgeDownloadParams = bridgeDownloadParametersService.getBridgeDownloadParams(scanParameters, bridgeDownloadParameters);
+
         int exitCode = -1;
 
-        if (scanStrategyService.isValidScanParameters(scanParameters)) {
+        if (scanStrategyService.isValidScanParameters(scanParameters) &&
+            bridgeDownloadParametersService.performBridgeDownloadParameterValidation(bridgeDownloadParams)) {
+            BridgeDownloadManager bridgeDownloadManager = new BridgeDownloadManager(workspace, listener);
+            boolean isBridgeDownloadRequired = bridgeDownloadManager.isSynopsysBridgeDownloadRequired(bridgeDownloadParams);
+
+            if (isBridgeDownloadRequired) {
+                bridgeDownloadManager.initiateBridgeDownloadAndUnzip(bridgeDownloadParams);
+            } else {
+                listener.getLogger().println("Bridge download is not required. Found installed in: " + bridgeDownloadParams.getBridgeInstallationPath());
+            }
+            FilePath bridgeInstallationPath = new FilePath(workspace.getChannel(), bridgeDownloadParams.getBridgeInstallationPath());
+
             try {
-                exitCode = scanner.runScanner(scanParameters, scanStrategyService);
+                exitCode = scanner.runScanner(scanParameters, scanStrategyService, bridgeInstallationPath);
             } catch (Exception e) {
-                e.printStackTrace();
+                e.printStackTrace(listener.getLogger());
                 throw new ScannerJenkinsException(ExceptionMessages.scannerFailureMessage(e.getMessage()));
             }
         }
