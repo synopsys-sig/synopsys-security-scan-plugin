dbaf787f9bea6457aea24ed3c1cbd02900cac2ba
jraihan <jahid.raihan@dsinnovators.com>
2023-08-24 19:11:31 +0600

    updated LICENSE and Eula.txt. added header comment.

--- src/main/java/com/synopsys/integration/jenkins/scan/bridge/BridgeDownloadManager.java
+++ src/main/java/com/synopsys/integration/jenkins/scan/bridge/BridgeDownloadManager.java
@@ -1,3 +1,10 @@
+/*
+ * synopsys-security-scan-plugin
+ *
+ * Copyright (c) 2023 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.jenkins.scan.bridge;
 
 import com.synopsys.integration.jenkins.scan.global.ApplicationConstants;
