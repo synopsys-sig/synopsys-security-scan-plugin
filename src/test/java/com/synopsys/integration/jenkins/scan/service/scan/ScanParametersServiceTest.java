bc91abbfa20ec4e0874989d59a39aacf87f1d6ec
Akib Uz Zaman <akibuz@synopsys.com>
2023-09-01 16:51:03 +0600

    update according to pr comments

--- src/test/java/com/synopsys/integration/jenkins/scan/service/scan/ScanParametersServiceTest.java
+++ src/test/java/com/synopsys/integration/jenkins/scan/service/scan/ScanParametersServiceTest.java
@@ -11,7 +11,7 @@ import org.mockito.Mockito;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ParametersServiceTest {
+public class ScanParametersServiceTest {
     private ScanParametersService scanParametersService;
     private final TaskListener listenerMock = Mockito.mock(TaskListener.class);
 
