c824ce672bc8ef4b0985781de9c3e2cb1667fc8d
Akib Uz Zaman <akibuz@synopsys.com>
2023-10-10 13:15:43 +0600

    fix warning for unsafe callable for checkRoles in OsNameTask

--- src/main/java/com/synopsys/integration/jenkins/scan/global/OsNameTask.java
+++ src/main/java/com/synopsys/integration/jenkins/scan/global/OsNameTask.java
@@ -10,15 +10,11 @@ package com.synopsys.integration.jenkins.scan.global;
 import hudson.FilePath;
 import hudson.remoting.VirtualChannel;
 import java.io.File;
-import org.jenkinsci.remoting.RoleChecker;
+import jenkins.MasterToSlaveFileCallable;
 
-public class OsNameTask implements FilePath.FileCallable<String> {
+public class OsNameTask extends MasterToSlaveFileCallable<String> implements FilePath.FileCallable<String> {
     @Override
     public String invoke(File workspace, VirtualChannel channel) {
         return System.getProperty("os.name").toLowerCase();
-    }
-
-    @Override
-    public void checkRoles(RoleChecker checker) throws SecurityException {
     }
 }
